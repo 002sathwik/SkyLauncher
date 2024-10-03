de2a3886a90ac79a6cd9fad8a731755e07d9fdb4
Sathwik <hsathwik002@gmail.com>
2024-10-03 17:46:50 +0530

    Feath:ClonedFiles/Each-File_Iteration_Fucntion

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,23 @@
 
 import express from 'express';
 import cors from 'cors';
-import bodyParser from 'body-parser';
+import path from 'path';
 import simpleGit from 'simple-git';
 import { generate } from './utils';
-
+// import {getAllFiles} from './files';
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.post("/deploy", async (req: any, res: any) => {
     const repoUrl = req.body.repoUrl;
     const id = generate();
-    await simpleGit().clone(repoUrl, `output/${id}`);
-    console.log(repoUrl);
+    await simpleGit().clone(repoUrl, path.join(__dirname, `output/${id}`));
+
+
+    // const files = getAllFiles(path.join(__dirname, `output/${id}`));
+    // console.log(files);
     res.json({
-        id:id
+        id: id
     })
 });
 
