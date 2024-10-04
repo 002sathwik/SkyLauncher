032adbe4ae88aa4d7acc6700252ab49e0ecc0191
Sathwik <hsathwik002@gmail.com>
2024-10-04 21:45:28 +0530

    Feath:Redis Init

--- uplode-service/src/index.ts
+++ uplode-service/src/index.ts
@@ -7,6 +7,15 @@ import { generate } from './utils';
 import { getAllFiles } from './files';
 import { UploadFileToS3 } from './uplodeToS3';
 import { sendMessageToSQS } from './awsSQSuplode';
+import { createClient } from "redis";
+
+
+const publisher = createClient();
+publisher.connect();
+
+const subscriber = createClient();
+subscriber.connect();
+
 
 const app = express();
 app.use(cors());
@@ -25,12 +34,22 @@ app.post("/deploy", async (req: any, res: any) => {
 
     });
     await sendMessageToSQS(id);
+
+    publisher.hSet("status", id, "uploded");
  
     console.log("done");
     res.json({
         id: id
     })
 });
+
+app.get("/status", async (req, res) => {
+    const id = req.query.id;
+    const response = await subscriber.hGet("status", id as string);
+    res.json({
+        status: response
+    })
+})
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
